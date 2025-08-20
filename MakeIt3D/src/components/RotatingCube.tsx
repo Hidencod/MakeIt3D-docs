6714d014c95f8b0c00e1fdcdf0f176f726787655
Maliaypogu Jammanna <jammanna017@gmail.com>
2025-08-20 18:54:04 +0530

    added imports for videos

--- MakeIt3D/src/components/RotatingCube.tsx
+++ MakeIt3D/src/components/RotatingCube.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./RotatingCube.css";
 
+import test from "./test.mp4"
+import raycastvideo from "./raycast.mp4"
+import logo from "../../static/img/logo.png"
+
 export default function RotatingCube() {
     return (
         <div className="cube-container">
             <div className="glow"></div>
             <div className="rotating-cube">
                 {/* Face 1 (stripes) */}
-                <div className="cube-face">3D</div>
+                <div className="cube-face"> <img src={logo} alt="logo" style={{ width: "80px", height: "80px" }} /></div>
 
                 {/* Face 2 (radial dots) → VIDEO */}
                 <div className="cube-face">
                     <video
-                        src="/test.mp4"
+                        src={test}
                         autoPlay
                         loop
                         muted
@@ -22,10 +26,18 @@ export default function RotatingCube() {
                 </div>
 
                 {/* Face 3 (split gradient) */}
-                <div className="cube-face">⚡</div>
+                <div className="cube-face"><video
+                    src={raycastvideo}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className="w-full h-full object-cover"
+                /></div>
 
                 {/* Face 4 (conic gradient) */}
-                <div className="cube-face">🎨</div>
+                <div className="cube-face">Test
+                </div>
 
                 {/* Face 5 (striped gradient with overlay) */}
                 <div className="cube-face">✨</div>
